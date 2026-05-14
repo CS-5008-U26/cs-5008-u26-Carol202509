e9c0b88738fc40d795802d100805879cbd81f484
wang.xin <wang.xin17@northeastern.edu>
2026-05-14 11:30:24 -0700

    Done task 3

--- Day 1/firstcprogram.c
+++ Day 1/firstcprogram.c
@@ -2,7 +2,7 @@
  *
  * This code will start you off.  Modify it as instructed in Homework 1.
  * 
- * Put your name here: Professor Shafer
+ * Put your name here: Carol(Xin Wang)
  * CS-5008 Spring 2025
  */
 
@@ -13,21 +13,20 @@
 
 int main() {
 
-    int x;                          // x will be the value entered by the user
-    char s[100];                    // s is used to "pause" the console so it doesn't exit too quickly
+    int x, y, sum;
+    char s[100];
 
-    /* Ask for a number and print it out */
-    printf("Enter a number: ");     // prompt the user
-    scanf("%d", &x);                // read a number from the console ("stdin")
-    fgets(s,100,stdin);             // sadly, we have to remove the \n that scanf leaves in stdin
-    printf("You entered %d\n", x);  // tell the user what number they just entered
+    printf("Enter a number: \n");
+    scanf("%d", &x);
+    fgets(s,100,stdin);
     
-    /* Normally I would not put a comment on every line of code.
-       I just did it above because you are learning C for the first time.
-       I would only put a comment on a line if it was hard to understand or required that you
-       know something that is not entirely obvious by looking at the code.
-    */
+    printf("Enter another number: \n");
+    scanf("%d", &y);
+    fgets(s,100,stdin);
 
-    /* Pause the console so we can look at it before it vanishes */
+    sum = x + y;
+
+    printf("Their sum is %d\n", sum);
+
     fgets(s,100,stdin);
 }
